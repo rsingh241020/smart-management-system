15a9a59c5adfa892902cad4327acfe3d5fe66b44
Trainee IT <trainee.it@greenpanel.com>
2026-05-04 17:57:39 +0530

    Initial backend setup with Spring Boot and PostgreSQL

---
+++ src/main/java/com/example/Smart/Management/System/SmartManagementSystemApplication.java
@@ -0,0 +1,15 @@
+package com.example.Smart.Management.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartManagementSystemApplication {
+
+	public static void main(String[] args) {
+
+        SpringApplication.run(SmartManagementSystemApplication.class, args);
+        System.out.println("SmartManagementSystemApplication Started");
+	}
+
+}
